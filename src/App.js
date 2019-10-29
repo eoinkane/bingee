e483881d3e6e933351cd6334b9e77fc9c0ca1408
Eoin Kane <eoinkane23@gmail.com>
2019-10-29 11:51:29 +0000

    Added navbar component, still working on integration of navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// CSS
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Navbar from "./components/Navbar/Navbar";
+
+class App extends Component {
+  state = {
+    position: "start"
+  }
+
+  render() {
+    return(
+      <div>
+        <Navbar full={true}/>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+function NavbarLocalFull(props) {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <form className="form-inline">
+                <h5 className="navBarH5">Bingee!</h5>
+                <div className="row" >
+                    <div className="col-sm-2 navBarBtnRow">
+                        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="homeBtn" type="button">
+                            Home
+                            <i className="fas fa-home homeIcon"></i>
+                        </button>
+                        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn disabled" type="button">Options</button>
+                        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="backBtn" type="button">
+                            <i className="fas fa-arrow-circle-left"></i>
+                        </button>
+                        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="upBtn" type="button">
+                            <i className="fas fa-arrow-circle-up"></i>
+                        </button>
+
+                    </div>
+                </div>
+            </form>
+        </nav>
+    )
+}
+function NavbarLocalSmall(props) {
+    return (
+        
+        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="downBtn" type="button">
+            <i className="fas fa-arrow-circle-down"></i>
+        </button>
+        
+       // <button type="button">but</button>
+    )
+}
+
+function Navbar(props) {
+    // /*
+    if (props.full) {
+        return (        
+            <NavbarLocalFull />       
+        )
+    } else if (!props.full) {
+        return (
+            <NavbarLocalSmall />
+        )
+    }
+    
+    // */
+    /*
+    return (
+        <NavbarLocalSmall />
+    )
+    */
+
+    
+}
+
+export default Navbar;
