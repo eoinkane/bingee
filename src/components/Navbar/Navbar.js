312d8b257167d5064ac94bda2d42bd94582aadfc
Eoin Kane <eoinkane23@gmail.com>
2019-10-30 11:43:40 +0000

    Navbar home button implemented
    Implemented through converting navbar to class based component
    
    Use lifecycle to remove then add event listeners when mounted and updated
    
    changePosition handler from App js is used
    
    possiblity to implement navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,87 @@
-import React from 'react';
-function NavbarLocalFull() {
+import React, { Component } from 'react';
+
+
+
+function NavbarLocalSmall() {
+    return (
+        
+        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="downBtn" type="button">
+            <i className="fas fa-arrow-circle-down"></i>
+        </button>
+        
+       // <button type="button">but</button>
+    )
+}
+
+class Navbar extends Component {
+    // handlers 
+    homeBtnHandler = () => {
+        //alert("Line 11");
+        //console.log(this.props);
+        this.props.handlers.changePositionHandler("Home");
+    }
+
+    addEventListenersHandler = () => {
+        //console.log("[Navbar.js] addEventListenersHandler");
+        document.querySelector("#homeBtn").addEventListener("click", this.homeBtnHandler);
+    }
+
+    removeEventListenersHandler = () => {
+        //console.log("[Navbar.js] removeEventListenersHandler");
+        document.querySelector("#homeBtn").removeEventListener("click", this.homeBtnHandler);
+    }
+
+    handlers = {
+        addEventListenersHandler: this.addEventListenersHandler,
+        removeEventListenersHandler: this.removeEventListenersHandler
+    }
+
+    // Lifecycle
+
+    componentDidMount() {
+        //console.log("[Navbar.js] componentDidMount");
+        this.handlers.addEventListenersHandler();
+    }
+
+    componentDidUpdate() {
+        //console.log("[Navbar.js] componentDidUpdate");
+        this.handlers.removeEventListenersHandler();
+        this.handlers.addEventListenersHandler();
+    }
+
+    render() {
+        // /*
+        if (this.props.full) {
+            return (
+                <NavbarLocalFull />
+            )
+        } else if (!this.props.full) {
+            return (
+                <NavbarLocalSmall />
+            )
+        }
+    }
+                
+    // */
+    /*
+    return (
+        <NavbarLocalSmall />
+    )
+    */
+
+    
+}
+
+const NavbarLocalFull = (() => {
     return (
         <nav className="navbar navbar-light bg-light">
             <form className="form-inline">
                 <h5 className="navBarH5">Bingee!</h5>
                 <div className="row" >
                     <div className="col-sm-2 navBarBtnRow">
-                        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn disabled" id="homeBtn" type="button">
+                        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="homeBtn" type="button">
                             Home
-                            <i className="fas fa-home homeIcon"></i>
+                        <i className="fas fa-home homeIcon"></i>
                         </button>
                         <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn disabled" type="button">Options</button>
                         <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn disabled" id="backBtn" type="button">
@@ -23,38 +96,6 @@ function NavbarLocalFull() {
             </form>
         </nav>
     )
-}
-function NavbarLocalSmall() {
-    return (
-        
-        <button className="btn btn-outline-success btn-sm my-2 my-sm-0 navBarBtn" id="downBtn" type="button">
-            <i className="fas fa-arrow-circle-down"></i>
-        </button>
-        
-       // <button type="button">but</button>
-    )
-}
-
-function Navbar(props) {
-    // /*
-    if (props.full) {
-        return (        
-            <NavbarLocalFull />       
-        )
-    } else if (!props.full) {
-        return (
-            <NavbarLocalSmall />
-        )
-    }
-    
-    // */
-    /*
-    return (
-        <NavbarLocalSmall />
-    )
-    */
-
-    
-}
+})
 
 export default Navbar;
